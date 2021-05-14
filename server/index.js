74faad20893530cac82e0eba77b1e38e7d5b8074
Asmin Shrestha <shrestha.asmin17@gmail.com>
2021-05-14 21:44:47 +0545

    memory app react plus node js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 app.use("/posts",postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://asushrestha:slap4msth@merncluster.ilnha.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://<username>:<password>@merncluster.ilnha.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
     .then(()=> app.listen(PORT,()=> console.log(`Server running on the port ${PORT}`)))
